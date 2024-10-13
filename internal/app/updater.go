2ff9501bcb32bdaa8159c3769a67d78de9213482
kynmh69 <hiroki.kayanuma@gmail.com>
2024-10-13 10:22:46 +0900

    #1 init pj

--- internal/app/updater.go
+++ internal/app/updater.go
@@ -2,25 +2,45 @@ package app
 
 import (
 	"github.com/kynmh69/gorutine-test/pkg/logging"
+	"os"
+	"os/signal"
 	"sync"
 	"time"
 )
 
 var counter int
 
-func Updater(wg *sync.WaitGroup) {
+func Updater() {
 	// update the app
+	var wg sync.WaitGroup
+	quit := make(chan struct{})
 	logger := logging.GetLogger()
 	logger.Infoln("start the updater")
 	wg.Add(1)
-	go updateToken(wg)
+	go updateToken(&wg, quit)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c)
+	<-c
+	close(quit)
+	wg.Wait()
+	logger.Infoln("stop the updater")
+	logger.Infof("exit program: %d\n", counter)
 }
 
-func updateToken(wg *sync.WaitGroup) {
+func updateToken(wg *sync.WaitGroup, quit chan struct{}) {
 	defer wg.Done()
 	logger := logging.GetLogger()
 	logger.Infoln("update the token")
-	time.Sleep(5 * time.Second)
-	counter++
-	logger.Infoln("update the token done")
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			counter++
+			logger.Infof("update the token %d times\n", counter)
+		case <-quit:
+			logger.Infoln("quit the updater")
+			return
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
-import "github.com/kynmh69/gorutine-test/pkg/logging"
+import (
+	"github.com/kynmh69/gorutine-test/internal/app"
+	"github.com/kynmh69/gorutine-test/pkg/logging"
+)
 
 func init() {
 	// init logger
 	logging.InitLogger()
 }
 func main() {
-	// init error group
-
+	app.Updater()
 }
